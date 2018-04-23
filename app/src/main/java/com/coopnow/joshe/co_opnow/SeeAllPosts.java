afc45301d92c4ec3b1665c5551af3e7f70f0a0a5
JoshuaEsquilin <38670056+JoshuaEsquilin@users.noreply.github.com>
2018-04-23 19:08:34 -0400

    Updated the UI

---
+++ app/src/main/java/com/coopnow/joshe/co_opnow/SeeAllPosts.java
@@ -0,0 +1,13 @@
+package com.coopnow.joshe.co_opnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeeAllPosts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_all_posts);
+    }
+}
