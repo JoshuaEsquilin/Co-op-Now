a8ac35116aba57dab91afa3d8d63c4b4572d25bf
JoshuaEsquilin <38670056+JoshuaEsquilin@users.noreply.github.com>
2018-04-23 16:38:04 -0400

    Co-op Now Inital State with Login
    Added Login support that connects to the database

---
+++ app/src/main/java/com/coopnow/joshe/co_opnow/GroupListing.java
@@ -0,0 +1,13 @@
+package com.coopnow.joshe.co_opnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_listing);
+    }
+}
