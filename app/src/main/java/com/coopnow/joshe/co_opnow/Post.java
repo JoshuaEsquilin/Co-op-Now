93e1f6dd40930838fe33a5d263650f6805c0ddb0
JoshuaEsquilin <38670056+JoshuaEsquilin@users.noreply.github.com>
2018-04-23 20:05:45 -0400

    Game Post and Comment Models have been added
    Added the basic Game Post and Comment Model

--- app/src/main/java/com/coopnow/joshe/co_opnow/Post.java
+++ app/src/main/java/com/coopnow/joshe/co_opnow/Post.java
@@ -3,11 +3,52 @@ package com.coopnow.joshe.co_opnow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Post extends AppCompatActivity {
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_info);
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class Post {
+
+    public String userID;
+    public String author;
+    public String gameName;
+    public String platform;
+    public String numOfPeople;
+    public String availability;
+    public String location;
+
+    public Post() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
     }
+
+    public Post(String userID, String author, String gameName, String platform, String numOfPeople, String availability, String location) {
+        this.userID = userID;
+        this.author = author;
+        this.gameName = gameName;
+        this.platform = platform;
+        this.numOfPeople = numOfPeople;
+        this.availability = availability;
+        this.location = location;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("userID", userID);
+        result.put("author", author);
+        result.put("gameName", gameName);
+        result.put("platform", platform);
+        result.put("numOfPeople", numOfPeople);
+        result.put("availability", availability);
+        result.put("location", location);
+
+
+        return result;
+    }
+
+
 }
+
